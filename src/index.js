4b6fcfadcbfe86545038fd19ed14f18075679c3d
kiran2000gk <42055649+kiran2000gk@users.noreply.github.com>
2021-06-11 12:26:05 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "react-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+import App from "./Components/App";
+
+render(<App />, document.getElementById("root"));
